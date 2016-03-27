c687f9cb61186f031679e5197e2ceed8bdd04dac
Norgerman <xyn0410@gmail.com>
2016-03-27 13:53:06 +0800

    add micro

--- include/make_unique.hpp
+++ include/make_unique.hpp
@@ -1,5 +1,10 @@
+#ifndef MAKE_UNIQUE_HPP
+#define MAKE_UNIQUE_HPP
+
 template<typename T, typename... Ts>
 std::unique_ptr<T> make_unique(Ts&&... params)
 {
-    return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
+	return std::unique_ptr<T>(new T(std::forward<Ts>(params)...));
 }
+
+#endif // !MAKE_UNIQUE_HPP
